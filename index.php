9a38779e256f35e1c495b8a44fd0b267d48aa051
SAMUEL EGAHI OKWORI <okworisamuel6@gmail.com>
2018-04-12 16:54:21 +0000

    initial commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN""http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="content-Type" content="text/html; charset=UTF-8" />
+        <title>Demystifying Email Design</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+    <body style="margin: 0; padding: 0;">
+        <table border="1" cellpadding="0" cellspacing="0" width="100%">
+            <tr>
+                <td>
+                    <table align="center" border="1" cellpadding="0" cellspacing="0" width="600" style="border-collapse: collapse;">
+                        <tr>
+                            <td bgcolor="#70bbd9" align="center" style="padding: 40px 0 30px 0;">
+                                <img src="https://cdn.tutsplus.com/webdesign/uploads/2013/06/6.jpg" alt="creating Email Magic" width="300" height="230" style="display: block;"/>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ffffff" style="padding: 40px 30px 40px 30px;">
+                                <table border="1" cellpadding="0" cellspacing="0" width="100%">
+                                    <tr>
+                                        <td>
+                                            Lorem ipsum dolor sit amet!
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td style="padding: 20px 0 30px 0;">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+                                        </td>
+                                    </tr>
+                                    <table border="1" cellpadding="0" cellspacing="0" width="100%">
+                                        <tr>
+                                            <td width="260" valign="top">
+                                                <table border="1" cellpadding="0" celllspacing="0" width="100%">
+                                                    <tr>
+                                                        <td width="260" valign="top">
+                                                            <table border="1" cellpadding="0" cellspacing="0" width="100%">
+                                                                <tr>
+                                                                    <td>
+                                                                        <img src="https://cdn.tutsplus.com/webdesign/uploads/2013/06/13.png" alt="" width="100%" height="140" style="display: block;"/>
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td style="padding: 25px 0 0 0;">
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+                                                                    </td>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                        <td style="font-size: 0; line-height: 0;" width="20">
+                                                            &nbsp;
+                                                        </td>
+                                                        <td width="260" valign="top">
+                                                            <table border='1' cellpadding="0" cellspacing="0" width="100%">
+                                                                <tr>
+                                                                    <td>
+                                                                        <img src="https://cdn.tutsplus.com/webdesign/uploads/2013/06/13.png" alt="" width="100%" height="140" style="display: block;" />
+                                                                    </td>
+                                                                </tr>
+                                                                <tr>
+                                                                    <td style="padding: 25px 0 0 0;">
+                                                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat.
+                                                                    </td>
+                                                                </tr>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </table>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td bgcolor="#ee4c50">Row 3</td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
